a9f1a1e3d9f0cd6b2fbb40ae34fbd3d97405a535
kusumachalasani <kchalasa@redhat.com>
2021-07-02 14:07:50 +0530

    Extra sleep time for deployments

--- techempower/scripts/perf/run-tfb-qrh-openshift.sh
+++ techempower/scripts/perf/run-tfb-qrh-openshift.sh
@@ -339,6 +339,8 @@ function runIterations() {
 			${SCRIPT_REPO}/tfb-qrh-deploy-openshift.sh -s ${BENCHMARK_SERVER} -n ${NAMESPACE} -i ${SCALING} -g ${TFB_IMAGE} --cpureq=${CPU_REQ} --memreq=${MEM_REQ} --cpulim=${CPU_LIM} --memlim=${MEM_LIM} --maxinlinelevel=${maxinlinelevel} --quarkustpcorethreads=${quarkustpcorethreads} --quarkustpqueuesize=${quarkustpqueuesize} --quarkusdatasourcejdbcminsize=${quarkusdatasourcejdbcminsize} --quarkusdatasourcejdbcmaxsize=${quarkusdatasourcejdbcmaxsize} >> ${LOGFILE}
 			# err_exit "Error: tfb-qrh deployment failed" >> ${LOGFILE}
 		fi
+		# Add extra sleep time for the deployment to complete as few machines takes longer time.
+		sleep 120
 		# Start the load
 		RESULTS_DIR_I=${RESULTS_DIR_R}/ITR-${itr}
 		echo "Running ${WARMUPS} warmups" >> ${LOGFILE}
